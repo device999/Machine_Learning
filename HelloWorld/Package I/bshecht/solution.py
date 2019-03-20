2662434f73ca3bc166af6898617590002735e62c
Aleksej Hecht <aleksej.hecht@plri.de>
2019-03-20 19:31:07 +0100

    1,2,3

--- HelloWorld/Package I/bshecht/solution.py
+++ HelloWorld/Package I/bshecht/solution.py
@@ -1,13 +1,30 @@
 import datetime
 
+#1
 def date_time():
     now = datetime.datetime.now()
     print ("Current date - time: ")
     print (now.strftime("%Y-%m-%d %H:%M:%S"))
 
+#2
+def accept_user_name():
+    first_name = input("Your first name: ")
+    last_name = input("Your last name: ")
+    print(last_name + " " + first_name)
+
+#3 
+def accept_inputed_filename():
+    file_name = input("Input the file name with extention: ")
+    splitting_array = file_name.split(".",1)
+    print("File extention: ",  splitting_array[1])
+
+#4
+
+# this is a main procedere for defined exersice  
 def main():
-    date_time()
-    
+#    date_time()
+#    accept_user_name()
+    accept_inputed_filename()
 
 if __name__=="__main__":
     main()
