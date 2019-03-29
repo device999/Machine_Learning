60a17d40d3b5eab4643033c44c5c4e97abacb8cc
Aleksej Hecht <aleksej.hecht@plri.de>
2019-03-29 16:19:03 +0100

    exersices coped

--- HelloWorld/Package II/bshecht/solution.py
+++ HelloWorld/Package II/bshecht/solution.py
@@ -32,6 +32,42 @@ def input_numbers():
 #            sum = summe(x)
 #    print("summe: ", sum)
 
+
+
+# 3. Write a Python program to find the number of divisors of a given integer is even or odd.
+
+# 4. Write a Python program to compute the summation of the absolute difference of all distinct pairs in an given array (non-decreasing order)
+
+# 5. Write a Python program to find common divisors between two numbers in a given pair.
+
+# 6. Write a Python program which solve the equation. (*)
+
+# ax+by=c
+# dx+ey=f
+# Print the values of x, y where a, b, c, d, e and f are given.
+# Input:
+# a,b,c,d,e,f separated by a single space.
+# (-1,000 = a,b,c,d,e,f = 1,000)
+# Input the value of a, b, c, d, e, f:
+# 5 8 6 7 9 4
+# Values of x and y:
+#-2.000 2.000
+
+# 7. Write a Python program to print the number of prime numbers which are less than or equal to an given integer.
+
+# n (1 = n = 999,999)
+# Input the number(n):
+# 35
+# Number of prime numbers which are less than or equal to n.: 11
+
+# 8. Write a Python program which reads a text (only alphabetical characters and spaces.) and prints two words. The first one is the word which is arise most frequently in the text. The second one is the word which has the maximum number of letters. 
+
+# 9. Write a Python program to compute the sum of first n given prime numbers.
+
+# 10. Write a Python program to print a long text, convert the string to a list and print all the words and their frequencies.
+
+
+
 # this is a main procedere for defined exersice  
 def main():
 #    OS_INFO()
